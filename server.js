5e92978bc71ddcfdf6dd81471d946b077bac5ac3
Max Lyuchin <maxim.lyuchin@gmail.com>
2022-05-23 15:06:02 +0300

    Initial version

---
+++ server.js
@@ -0,0 +1,25 @@
+const { isProbablyReaderable, Readability } = require("@mozilla/readability");
+const express = require("express");
+var bodyParser = require("body-parser");
+
+const { JSDOM } = require("jsdom");
+const app = express();
+const port = 5052;
+
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
+
+app.post("/read", (req, res, next) => {
+  const url = req.body?.url;
+  const html = req.body?.html;
+
+  const doc = new JSDOM(html, { url: url });
+
+  if (isProbablyReaderable(doc.window.document)) {
+    const article = new Readability(doc.window.document).parse();
+    res.json(article);
+  } else {
+    res.json({ error: true, message: "Document is not readable" });
+  }
+});
+
+app.listen(port);
