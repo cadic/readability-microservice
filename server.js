0534b9a0db93e6c1c45a155775ee24fb6690c80c
Max Lyuchin <maxim.lyuchin@gmail.com>
2022-05-23 21:31:10 +0300

    Support env variables, add api key support

--- server.js
+++ server.js
@@ -6,24 +6,39 @@ const yargs = require("yargs");
 
 const app = express();
 
-const argv = yargs.option("port", {
-  alias: "p",
-  description: "Specify port",
-  type: Number,
-  default: 5052,
-}).argv;
+const argv = yargs
+  .option("port", {
+    alias: "p",
+    description: "Specify port",
+    type: Number,
+    default: process.env.READABILITY_PORT ?? 5052,
+  })
+  .option("key", {
+    alias: "k",
+    description: "API key",
+    type: String,
+    default: process.env.READABILITY_API_KEY ?? null,
+  }).argv;
 
 const port = argv.port;
+const key = argv.key;
 
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
 
 app.post("/read", (req, res) => {
+  if (key) {
+    if (req.get("key") !== key) {
+      res.json({ error: true, message: "Worng API key" });
+      return;
+    }
+  }
+
   const url = req.body?.url;
   const html = req.body?.html;
 
   if (!html || !url) {
     res.json({ error: true, message: "Wrong request" });
-	return;
+    return;
   }
 
   const doc = new JSDOM(html, { url: url });
